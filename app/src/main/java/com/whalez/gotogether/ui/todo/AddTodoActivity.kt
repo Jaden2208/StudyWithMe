00f8b6e4105becbf262712d92aece7a9e848a5c1
ryu22 <ryu2208@naver.com>
2020-04-17 21:04:15 +0900

    날짜 선택 기능 구현
    구현 예정 기능
    - 수정.
    - 홈에서 오늘 할 일 보여주기. (리스트로)
    - Group Fragment(Firebase)
     + 그룹 찾기.
     + 그룹 만들기.
     + 내 그룹 목록 (맨 위에 배치)

--- app/src/main/java/com/whalez/gotogether/ui/todo/AddTodoActivity.kt
+++ app/src/main/java/com/whalez/gotogether/ui/todo/AddTodoActivity.kt
@@ -1,35 +1,52 @@
 package com.whalez.gotogether.ui.todo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import com.whalez.gotogether.EXTRA_CONTENT
 import com.whalez.gotogether.EXTRA_TIMESTAMP
 import com.whalez.gotogether.R
 import com.whalez.gotogether.shortToast
 import kotlinx.android.synthetic.main.activity_add_todo.*
 import org.joda.time.DateTime
+import java.util.*
 
 class AddTodoActivity : AppCompatActivity() {
-
-//    private val contentList = ArrayList<String>()
-//    // 어댑터 부터 만들기.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
 
+        Log.d("kkk", "AddTodoActivity. onCreate()")
+
         var timestamp = DateTime().millis
-        tv_year_month_date.text = DateTime(timestamp).toString("yyyy년 MM월 dd일")
-        tv_year_month_date.setOnClickListener {
-            // 날짜 수정.
+        btn_show_date_picker.text = DateTime().toString("yyyy년 MM월 dd일")
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+//        btn_show_date_picker.text = "${year}년 ${month}월 ${day}일"
+        btn_show_date_picker.setOnClickListener {
+            val dpd = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { view, year, month, day ->
+                    val changedTime = DateTime(year, month + 1, day, 0, 0, 0)
+                    timestamp = changedTime.millis
+                    btn_show_date_picker.text = changedTime.toString("yyyy년 MM월 dd일")
+                },
+                year,
+                month,
+                day
+            )
+            dpd.show()
         }
 
         btn_back.setOnClickListener { finish() }
 
         btn_save.setOnClickListener {
             val content = tv_content.text.toString()
-            if(content.isEmpty()) {
+            if (content.isEmpty()) {
                 shortToast(this, "저장할 내용이 없습니다!")
                 return@setOnClickListener
             }
@@ -40,4 +57,5 @@ class AddTodoActivity : AppCompatActivity() {
         }
 
     }
+
 }
